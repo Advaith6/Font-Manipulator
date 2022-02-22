dfbca0d2d60d978a89fd4cb50d1a414c803e9115
Advath Coder <72031535+Advaith6@users.noreply.github.com>
2022-02-22 17:39:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -11,6 +14,10 @@ function setup() {
 
 function draw() {
     background('#969A98');
+    document.getElementById("font_size").innerHTML = "Font size of the text will be = " + difference + "px";
+    fill('#F90093');
+    textSize(difference);
+    text('Advaith Coder', 50, 400)
 }
 
 function modelLoaded() {
@@ -20,5 +27,9 @@ function modelLoaded() {
 function gotPoses() {
     if (results.length > 0) {
         console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX + "rightWristX= " + rightWristX + "difference = " + difference);
     }
 }
